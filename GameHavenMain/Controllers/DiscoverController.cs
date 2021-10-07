e934c280c428d6675d706c70eb80a39181e5377a
Jorrit Deschaux <458871@student.fontys.nl>
2021-10-07 16:10:13 +0200

    Created first controller, created base dbcontext class and some model classes

---
+++ GameHavenMain/Controllers/DiscoverController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameHavenMain.Controllers
+{
+	public class DiscoverController : Controller
+	{
+		public IActionResult Index()
+		{
+			return Ok();
+		}
+	}
+}
---
+++ GameHavenMain/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameHavenMain.Data
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<GameReview> GameReview { get; set; }
+		public DbSet<User> User { get; set; }
+
+	}
+}
